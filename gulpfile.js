1eb3b4539637e017edec6b04dd1330d85ea326be
wan <tshina17@yahoo.com.hk>
2015-03-11 10:57:40 -0700

    Added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var gulp = require('gulp'),
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = ['components/scripts/*.js'];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -34,13 +36,15 @@ gulp.task('compass', function() {
 		comments: true
 	}).on('error', gutil.log))
 	.pipe(gulp.dest('builds/development/css'))
-	.pipe(connect.reload());;
+	.pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function() {
@@ -50,4 +54,14 @@ gulp.task('connect', function() {
 	});
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+	.pipe(connect.reload());
+});
+
+gulp.task('json', function() {
+	gulp.src(jsonSources)
+	.pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
